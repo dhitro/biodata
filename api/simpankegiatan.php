07a9b4a7672c024f1c81de7ea7a456bcb0dbbf75
didit <dhitro@gmail.com>
2021-01-21 17:45:58 +0700

    cek upload file

--- api/simpankegiatan.php
+++ api/simpankegiatan.php
@@ -22,7 +22,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $stat_survey = 0;
     // var_dump($_FILES['image']['name']);
 
-
     $sql = "INSERT INTO kegiatan VALUE(NULL,'$kegiatan','$uraian','$jenis','$permasalahan',$volume,'$satuan',$long,$lat,'$alamat','$kondisi',NULL,'$keterangan',$id_unit,$id_skpd,$stat_verif,$stat_survey)";
 
     // $res = mysqli_fetch_array($run);
@@ -33,21 +32,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             // foreach ($_FILES["image"]["tmp_name"] as $key => $tmp_name) {
             $image = date('dmYHis') . str_replace(" ", "", basename($_FILES['image']['name'][$i]));
             // $path = dirname(__FILE__) . "/upload/" . $image;
-            $path = "upload/" . $image;
+            $path = __DIR__ . "/upload/" . $image;
             $ext = explode('.', basename($_FILES['image']['name'][$i])); //explode file name from dot(.) 
             $file_extension = end($ext); //store extensions in the variable
 
             // if (!file_exists("upload/" . $image)) {
             // move_uploaded_file($file_tmp=$_FILES["files"]["tmp_name"][$i],"photo_gallery/".$txtGalleryName."/".$file_name);
-            $apt = move_uploaded_file($_FILES['image']['tmp_name'][$i], "upload/" . $image);
-            if ($apt) {
-                $sql_file = "INSERT INTO file VALUE(NULL,$last_id,'$image','$file_extension',1)";
-                if (mysqli_query($con, $sql_file)) {
-                }
+            @move_uploaded_file($_FILES['image']['tmp_name'][$i], getcwd() . DIRECTORY_SEPARATOR . "upload/" . $image);
+            // if ($apt) {
+            $sql_file = "INSERT INTO file VALUE(NULL,$last_id,'$image','$file_extension',1)";
+            if (mysqli_query($con, $sql_file)) {
             }
             // }
         }
-
 
         $key['msg'] = 1;
         $key['id'] = $last_id;
